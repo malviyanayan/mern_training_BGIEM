5379bccc9ff06cfb65f439b95b430595c1c432b4
nayan malviya <naymalviya@gmail.com>
2024-11-14 15:29:30 +0530

    14/11/2024

--- rp_14_11/B.js
+++ rp_14_11/B.js
@@ -34,4 +34,31 @@ console.log(getNumberFromArr());
 
 
 
+// console.log((null + null) === undefined);
+// console.log((null + null) === 0);
+// console.log(undefined == undefined)
 
+
+// console.log("one")
+// console.log("Two")
+
+
+
+
+// Asynchrounus programming
+
+// SetTimeOut Function
+
+// setTimeout(() => {
+//     console.log("one")
+// }, 2000);
+
+// console.log("Two")
+
+setInterval(() => {
+    console.log("jai Kishan");
+    setTimeout(() => {
+        console.log("jai javan ......");
+    }, 500);
+    console.log("-------------------");
+}, 1000);
